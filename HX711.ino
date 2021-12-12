07e8e692533c22d00fd1f0d5fb808c4a0f3a47d7
Martin Verges <martin@verges.cc>
2021-12-12 10:35:32 +0100

    working prototype

---
+++ HX711.ino
@@ -0,0 +1,197 @@
+// Fix an issue with the HX711 library on ESP32
+#undef ARDUINO_ARCH_ESP32
+#define ARDUINO_ARCH_ESP32 true
+
+#include <EEPROM.h>
+
+#include "HX711.h"
+HX711 hx711;
+
+struct Button {
+  const uint8_t PIN;
+  bool pressed;
+};
+
+Button button1 = {21, false};              // Run the setup 
+Button button2 = {22, false};              // unused
+
+const int GPIO_HX711_DOUT = 26;            // GPIO pin to use for DT or DOUT
+const int GPIO_HX711_SCK = 25;             // GPIO pin to use for SCK
+
+const float LOWER_END = 0.010;             // value increase to start recording (tank is empty)
+const float UPPER_END = 0.990;             // value limit to cutoff data (tank is full)
+
+float levelConfig[100] = {0};              // pressure readings to map to percentage filling
+int currentState = 0;                      // last reading in percent
+unsigned long millisStarted = 0;           // timer to run parts only at given interval
+
+const int MAX_DATA_POINTS = 255;           // how many level data points to store (increased accuracy)
+struct state_t {
+  int valueCount = 0;                      // current number of entries in readings[]
+  float minValue = 0.00;                   // lowest value to start recording
+  float lastread = 0.00;                   // last redading while in setup  
+  float readings[MAX_DATA_POINTS] = {0};   // data readings from pressure sensor while running level setup
+} setupConfig;
+
+
+
+
+void printData(float* readings, size_t count) {
+  for (uint8_t i = 0; i < count; i++) Serial.printf("%d = %f\n", i, readings[i]);
+}
+
+int getPercentage(float val) {
+  for(int x=99; x>0; x--) {
+    if (val > levelConfig[x]) return x+1;
+  }
+  return 0;
+}
+
+// Search through the setupConfig sensor readings and find the upper limit cutoff index
+int findEndCutoffIndex() {
+  float endCutoff = (setupConfig.readings[MAX_DATA_POINTS-1] - setupConfig.readings[0]) * UPPER_END + setupConfig.readings[0];
+  int endIndex = -1;
+  for (uint8_t i = 0; i < MAX_DATA_POINTS; i++) {
+    if (setupConfig.readings[i] > endCutoff) {
+      endIndex = i;
+      break;
+    }
+  }
+  return endIndex;
+}
+
+// Search through the setupConfig sensor readings and find the lower limit cutoff index
+int findStartCutoffIndex(int endIndex) {
+  float startCutoff = (setupConfig.readings[endIndex] - setupConfig.readings[0]) * LOWER_END + setupConfig.readings[0];
+  int startIndex = -1;
+  for (uint8_t i = 0; i < MAX_DATA_POINTS; i++) {
+    if (setupConfig.readings[i] < startCutoff) startIndex = i;
+    else break;
+  }
+  return startIndex;
+}
+
+void levelSetup() {
+  if (setupConfig.readings[0] <= 0) {  // Start the level setup
+    setupConfig.valueCount = 0;
+    setupConfig.readings[setupConfig.valueCount++] = hx711.get_max_value(10);
+    Serial.printf("Begin level setup with minValue of %f\n", setupConfig.readings[0]);
+    return;
+  }
+  if (setupConfig.valueCount >= MAX_DATA_POINTS) {
+    Serial.println("Exiting setup");
+
+    std::sort(std::begin(setupConfig.readings), std::end(setupConfig.readings));
+    // printData(setupConfig.readings, MAX_DATA_POINTS);
+    int endIndex = findEndCutoffIndex();
+    int startIndex = findStartCutoffIndex(endIndex);
+    // Serial.printf("Start Index = %d\n", startIndex);
+    // Serial.printf("End Index = %d\n", endIndex);
+
+    // calculate the percentages
+    int readingCount = endIndex - startIndex + 1;  // number of readings to use
+    int x = startIndex;                        // index of reading[]
+    float y1 = 0.00;                           // lower percentage of reading[] index (e.g. entry x equals 55%)
+    float y2 = 0.00;                           // upper percentage of reading[] index (e.g. entry x+1 equals 58%)
+    int Y = 0;                                 // % value (1-100%) 
+    if (Y==0) levelConfig[Y++] = setupConfig.readings[x];
+    while(Y <= 100) {
+      while (Y > y2 && x <= endIndex) {
+        // find the next dataset to calculate Z
+        y1 = (float)(x-startIndex  ) / readingCount * 100;     // lower percentage of this sensor reading
+        y2 = (float)(x-startIndex+1.0) / readingCount * 100;     // upper percentage of the next sensor reading
+        x++;
+      }
+      float Z = setupConfig.readings[x-1] + ((Y - y1) / (y2 - y1) * (setupConfig.readings[x] - setupConfig.readings[x-1])); // save the value into the configuration
+      levelConfig[Y] = Z;
+      // Serial.printf("\t\t\ty1=\t%f\t | y2=\t%f\n", y1, y2);
+      // Serial.printf("Y=%d\t| Z = %d | z1=\t%f\t| z2=\t%f\n", Y, (int)Z, setupConfig.readings[x-1], setupConfig.readings[x]);
+      Y++;
+    }
+    // printData(levelConfig, 100);
+    EEPROM.put(0, true);
+    EEPROM.put(sizeof(bool), levelConfig);
+
+    setupConfig.readings[0] = 0;
+    setupConfig.valueCount = 0;
+    return;
+  }
+  setupConfig.lastread = hx711.get_median_value(10);
+  Serial.printf("Recording new entry with a value of %f\n", setupConfig.lastread);
+  setupConfig.readings[setupConfig.valueCount++] = setupConfig.lastread;
+}
+
+void IRAM_ATTR ISR_button1() {
+  button1.pressed = true;
+}
+void IRAM_ATTR ISR_button2() {
+  button2.pressed = true;
+}
+
+void setup() {
+  Serial.begin(115200);
+  hx711.begin(GPIO_HX711_DOUT, GPIO_HX711_SCK, 32);
+
+  bool setupDone = false;
+  EEPROM.get(0, setupDone);
+  if (setupDone) {
+    Serial.println("Restoring levelData from EEPROM...");
+    EEPROM.get(sizeof(bool), levelConfig);
+    printData(levelConfig, 100);
+  } else {
+    Serial.println("no stored configuration found on EEPROM...");
+  }
+
+  pinMode(button1.PIN, INPUT_PULLUP);
+  attachInterrupt(button1.PIN, ISR_button1, FALLING);
+  pinMode(button2.PIN, INPUT_PULLUP);
+  attachInterrupt(button2.PIN, ISR_button2, FALLING);
+}
+
+/*
+UNITS = one reading: 4768.1  | average:  4768.9
+VALUE = one reading:  477464.0  | average:  476669.0
+RAW   = one reading:  4316381 | average:  4315608
+ */
+void readPressure() {
+  if (hx711.wait_ready_retry(100)) {
+    currentState = getPercentage(hx711.get_median_value(10));
+    Serial.printf("Tank fill status = %d\n", currentState);
+  } else {
+    Serial.println("Unable to communicate with the HX711 modul.");
+  }
+}
+
+void loop() {
+  if (button1.pressed) {
+    if (setupConfig.readings[0] > 0) {
+      Serial.println("User requested end of setup...");
+      while(setupConfig.valueCount < MAX_DATA_POINTS) {
+        setupConfig.readings[setupConfig.valueCount++] = setupConfig.lastread;
+      }
+    }
+    levelSetup();
+    button1.pressed = false;
+  }
+
+  if (button2.pressed) {
+    button2.pressed = false;
+    printData(levelConfig, 100);
+  }
+  
+  if (setupConfig.readings[0] > 0) {
+    // run the level setup
+    if (millis() - millisStarted >= 100) {
+      millisStarted = millis();
+      levelSetup();
+    }
+  } else {
+    // run regular operation
+    if (millis() - millisStarted > 1000) {
+      millisStarted = millis();
+      if (levelConfig[0] > 0) readPressure();
+      else Serial.println("No leveldata found, please run the setup to use the device!");
+    }
+  }
+  delay(25);
+}
--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # rv-smart-tanksensor
+
+[![GitHub Super-Linter](https://github.com/MartinVerges/rv-smart-tanksensor/workflows/Lint%20Code%20Base/badge.svg)](https://github.com/marketplace/actions/super-linter)
+
 DIY project to build a smart fuel sensor for RVs or in other projects. It is based on air pressure and is able to measure fresh water as well as grey and black water levels with high precision.
+
+This project is still in development, it's a rough prototype and I'm happy about pull requests to add more functionality, improve existing ones or just feedback.
+
+## Why?
+
+After I have long found on the market for camping nothing that is on the one hand affordable and on the other hand also technically good, I have started the development of my own tank sensor.
+
+Thanks to the ESP32 this can be queried in the future directly via WLAN or Bluetooth (work in progress). 
+
+## Focus of this Project
+
+The following aspects are the focus of this project:
+
+ * Easy to use and rebuild
+ * Precise in the evaluation of the data
+ * Stand alone usable
+ * Usable in all liquids
+
+## BOM - Bill of Materials
+
+To build this sensor yourself, you need:
+
+ 1x ESP32 ESP32-WROOM-32 (between 1.75€ and 8.00€)
+ 1x MPX53DP Pressure Sensor (~ 8.00€)
+ 1x HX711 24-bit ADC (~2.50€)
+ 1x Pushbutton (~0.15€)
+ 1x Tube with 4mm inner width (~1€) 
+ 
+ In addition you need some few small cables and soldering equipment to build the circuit.
+
+## Alternatives
+
+### Pressure based (like this project)
+
+The only know alternative SuperSense tank sensor is available at around 259€ for purchase, but the price is extremely high and the data cannot be accessed digitally as it's proprietary.
+
+### Resistance based
+
+There are a lot of cheapest probes which all show only rough and mostly wrong data. These are mostly based on measuring the resistance or continuity between two or more conductive rods that come into contact with water.
+
+These probes usually cost more than the components in this project, but are extremely inaccurate, prone to calcification and malfunction.
+
+# License
+
+This project and all of it's deliverables are free to use for every private person in his own private projects.
+If you wan to use it in comercial projects, you have to ask for permission!
