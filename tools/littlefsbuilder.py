1315108215b82c2248b165cd35184ed6e2389fc1
Martin Verges <martin@verges.cc>
2021-12-24 10:48:28 +0100

    fix lint for python

--- tools/littlefsbuilder.py
+++ tools/littlefsbuilder.py
@@ -1,20 +1,21 @@
-Import("env")
 from sys import platform
-import os 
+import os
 import stat
 
-if platform == "linux" or platform == "linux2":
-  file = env.get("PROJECT_DIR") + '/tools/mklittlefs_linux'
-  print("Replace MKSPIFFSTOOL with " + file)
-  st = os.stat(file)
-  os.chmod(file, st.st_mode | stat.S_IEXEC)
-elif platform == "darwin":
-  file = env.get("PROJECT_DIR") + '/tools/mklittlefs_darwin'
-  print("Replace MKSPIFFSTOOL with " + file)
-  st = os.stat(file)
-  os.chmod(file, st.st_mode | stat.S_IEXEC)
-elif platform == "win32":
-  file = env.get("PROJECT_DIR") + '/tools/mklittlefs_win.exe'
-  print("Replace MKSPIFFSTOOL with " + file)
+Import("env")
 
-env.Replace( MKSPIFFSTOOL=file )
+if platform == "linux" or platform == "linux2":
+    file = env.get("PROJECT_DIR") + '/tools/mklittlefs_linux'
+    print("Replace MKSPIFFSTOOL with " + file)
+    st = os.stat(file)
+    os.chmod(file, st.st_mode | stat.S_IEXEC)
+elif platform == "darwin":
+    file = env.get("PROJECT_DIR") + '/tools/mklittlefs_darwin'
+    print("Replace MKSPIFFSTOOL with " + file)
+    st = os.stat(file)
+    os.chmod(file, st.st_mode | stat.S_IEXEC)
+elif platform == "win32":
+    file = env.get("PROJECT_DIR") + '/tools/mklittlefs_win.exe'
+    print("Replace MKSPIFFSTOOL with " + file)
+
+env.Replace(MKSPIFFSTOOL=file)
