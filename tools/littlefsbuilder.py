d29a1d2bcdc1e06f51418455db2cd0464864fd53
Martin Verges <martin@verges.cc>
2021-12-24 11:33:41 +0100

    yet another lint fix

--- tools/littlefsbuilder.py
+++ tools/littlefsbuilder.py
@@ -8,17 +8,17 @@ from sys import platform
 Import("env")
 
 if platform == "linux" or platform == "linux2":
-    file = env.get("PROJECT_DIR") + '/tools/mklittlefs_linux'
+    file = env.get("PROJECT_DIR") + "/tools/mklittlefs_linux"
     print("Replace MKSPIFFSTOOL with " + file)
     st = os.stat(file)
     os.chmod(file, st.st_mode | stat.S_IEXEC)
 elif platform == "darwin":
-    file = env.get("PROJECT_DIR") + '/tools/mklittlefs_darwin'
+    file = env.get("PROJECT_DIR") + "/tools/mklittlefs_darwin"
     print("Replace MKSPIFFSTOOL with " + file)
     st = os.stat(file)
     os.chmod(file, st.st_mode | stat.S_IEXEC)
 elif platform == "win32":
-    file = env.get("PROJECT_DIR") + '/tools/mklittlefs_win.exe'
+    file = env.get("PROJECT_DIR") + "/tools/mklittlefs_win.exe"
     print("Replace MKSPIFFSTOOL with " + file)
 
 env.Replace(MKSPIFFSTOOL=file)
