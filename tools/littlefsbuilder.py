c4326ec8089593611e2eafc88737ee8e388bcf42
Martin Verges <martin@verges.cc>
2021-12-24 10:39:15 +0100

    updated mklittlefs for windows, linux, and darwin

--- tools/littlefsbuilder.py
+++ tools/littlefsbuilder.py
@@ -1,3 +1,20 @@
 Import("env")
-print("Replace MKSPIFFSTOOL with mklittlefs.exe")
-env.Replace( MKSPIFFSTOOL=env.get("PROJECT_DIR") + '/tools/mklittlefs' )
+from sys import platform
+import os 
+import stat
+
+if platform == "linux" or platform == "linux2":
+  file = env.get("PROJECT_DIR") + '/tools/mklittlefs_linux'
+  print("Replace MKSPIFFSTOOL with " + file)
+  st = os.stat(file)
+  os.chmod(file, st.st_mode | stat.S_IEXEC)
+elif platform == "darwin":
+  file = env.get("PROJECT_DIR") + '/tools/mklittlefs_darwin'
+  print("Replace MKSPIFFSTOOL with " + file)
+  st = os.stat(file)
+  os.chmod(file, st.st_mode | stat.S_IEXEC)
+elif platform == "win32":
+  file = env.get("PROJECT_DIR") + '/tools/mklittlefs_win.exe'
+  print("Replace MKSPIFFSTOOL with " + file)
+
+env.Replace( MKSPIFFSTOOL=file )
