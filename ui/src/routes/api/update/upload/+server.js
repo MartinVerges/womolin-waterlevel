76566b33a4ba60b149a86b7c8d41fad5c083b03f
Martin Verges <martin.verges@croit.io>
2023-01-04 15:17:03 +0100

    add api ui testing endpoint

---
+++ ui/src/routes/api/update/upload/+server.js
@@ -0,0 +1,13 @@
+import { error } from '@sveltejs/kit';
+
+/** @type {import('./$types').RequestHandler} */
+export async function POST({ request }) {
+	let data = await request.json();
+
+	if (!Object.prototype.hasOwnProperty.call(data, 'otaPassword')) {
+		throw error(422, JSON.stringify({ message: 'Invalid data' }));
+	}
+
+	let responseBody = { message: "Please wait while the device reboots!" };
+	return new Response(JSON.stringify(responseBody), { status: 200 });
+}
